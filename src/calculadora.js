9275c0d9b254fcc784f1fba2f462dd3ba1a37027
Sergio <143109097+DeimaxRow1342@users.noreply.github.com>
2024-05-13 22:27:36 -0700

    Cuando se envian dos numeros, se retorna la suma de estos

--- src/calculadora.js
+++ src/calculadora.js
@@ -1,8 +1,15 @@
 function sumar(cadenaNumeros) {
+  let suma = 0;
+  
   if(cadenaNumeros.length === 0){
     return 0;
   }
-  return cadenaNumeros[0];
+
+  else if(cadenaNumeros.length === 1){
+    return cadenaNumeros[0];
+  }
+
+  return cadenaNumeros[0] + cadenaNumeros[1];
 }
 
 export default sumar;
--- src/calculadora.spec.js
+++ src/calculadora.spec.js
@@ -8,4 +8,8 @@ describe("Calcular la suma de una cadena de numeros", () => {
   it("Si se envia una cadena con un numero, se deberia retornar ese mismo numero", () => {
     expect(sumar([5])).toEqual(5);
   });
+
+  it("Si se envia una cadena con dos numeros, se deberia retornar la suma de estos numeros", () => {
+    expect(sumar([2,4])).toEqual(6);
+  });
 });
